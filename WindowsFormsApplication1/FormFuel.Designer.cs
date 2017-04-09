759796f9e1cd0b490d328f5d341d9c588f7adf42
olgasn <olgasn@inbox.ru>
2017-04-09 21:56:07 +0300

    Некоторые доработки

--- WindowsFormsApplication1/FormFuel.Designer.cs
+++ WindowsFormsApplication1/FormFuel.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace WindowsFormsADO
 {
     partial class FormFuel
     {
--- WindowsFormsApplication1/FormFuel.cs
+++ WindowsFormsApplication1/FormFuel.cs
@@ -3,14 +3,16 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace WindowsFormsADO
 {
     public partial class FormFuel : Form
     {
-
-        SqlDataAdapter dataAdapter = new SqlDataAdapter();
         DataSet ds = new DataSet();
+        SqlDataAdapter dataAdapter;
+        SqlCommandBuilder builder;
+        string queryString = "SELECT * FROM Fuels";
         string ConnectionString = @"Data Source =.\sqlexpress;Initial Catalog = toplivo; Integrated Security = True";
+
         public FormFuel()
         {
             InitializeComponent();
@@ -23,7 +25,6 @@ namespace WindowsFormsApplication1
             SqlConnection conn = new SqlConnection(ConnectionString);
             labelInfo.Text = "\r\n Ход выполнения процесса визуализации:\r\n";
             labelInfo.Refresh();
-
             try
             {
                 conn.Open();
@@ -39,13 +40,13 @@ namespace WindowsFormsApplication1
 
                 MyCommand.CommandText = "SELECT * FROM Fuels Where FuelType LIKE '%' +@FindFuelType +'%'";
                 MyCommand.Parameters.AddWithValue("@FindFuelType", FindFuelType);
+
+                dataAdapter = new SqlDataAdapter();
                 dataAdapter.SelectCommand = MyCommand;
                 dataAdapter.Fill(ds, "Fuels");
 
-
                 labelInfo.Text = labelInfo.Text + "3. отбор данных в локальное хранилище закончен;\r\n";
                 labelInfo.Refresh();
-
                 dataGridViewFuels.DataSource = ds.Tables["Fuels"].DefaultView;
 
                 labelInfo.Text = labelInfo.Text + "4. отображение данных из локального хранилища в табличных элементах управления закончено!!!\r\n";
@@ -66,29 +67,24 @@ namespace WindowsFormsApplication1
         }
 
 
-
         private void buttonDisplay_Click(object sender, EventArgs e)
         {
-            DisplayFuels(textBoxFindFuels.Text);      
-      
+            DisplayFuels(textBoxFindFuels.Text);     
         }
 
         private void buttonUpdate_Click(object sender, EventArgs e)
         {
             // Создание подключения
-
             SqlConnection conn = new SqlConnection(ConnectionString);
             
             try
             {
-                conn.Open();
+                conn.Open();          
 
-                
-
-                // Создать DbCommandBuilder.
-                SqlCommandBuilder builder = new SqlCommandBuilder();
+                // Создать CommandBuilder.
+                builder = new SqlCommandBuilder();
                 builder.DataAdapter = dataAdapter;
-                // Получить команду на обновление
+                // Получить сгенерированную команду на обновление
                 dataAdapter.UpdateCommand = builder.GetUpdateCommand();
                 DataTable table = ds.Tables["Fuels"];
                 dataAdapter.Update(table.Select(null, null,DataViewRowState.ModifiedCurrent));
@@ -118,8 +114,6 @@ namespace WindowsFormsApplication1
             {
                 using (conn)
                 {
-                    // Определение строки запроса
-                    string queryString = "SELECT * FROM Fuels";
 
                     // Создать команду на выборку
                     SqlCommand command = new SqlCommand();
@@ -127,14 +121,14 @@ namespace WindowsFormsApplication1
                     command.Connection = conn;
 
                     // Создать DataAdapter.
-                    SqlDataAdapter adapter = new SqlDataAdapter();
-                    adapter.SelectCommand = command;
+                    dataAdapter = new SqlDataAdapter();
+                    dataAdapter.SelectCommand = command;
 
                     // Создать CommandBuilder.
-                    SqlCommandBuilder builder = new SqlCommandBuilder();
-                    builder.DataAdapter = adapter;
-                    // Получить команду на удаление
-                    adapter.DeleteCommand = builder.GetDeleteCommand();
+                    builder = new SqlCommandBuilder();
+                    builder.DataAdapter = dataAdapter;
+                    // Получить сгенерированную команду на удаление
+                    dataAdapter.DeleteCommand = builder.GetDeleteCommand();
 
                     DataTable table = ds.Tables["Fuels"];
 
@@ -144,7 +138,7 @@ namespace WindowsFormsApplication1
                     {
                         row.Delete();
                     }
-                    adapter.Update(table);
+                    dataAdapter.Update(table);
                     table.AcceptChanges();
 
                 }
@@ -172,33 +166,32 @@ namespace WindowsFormsApplication1
             {
                 using (conn)
                 {
-                    // Определение строки запроса
-                    string queryString = "SELECT * FROM Fuels";
-
                     // Создать команду на выборку
                     SqlCommand command = new SqlCommand();
                     command.CommandText = queryString;
                     command.Connection = conn;
 
                     // Создать DataAdapter.
-                    SqlDataAdapter adapter = new SqlDataAdapter();
-                    adapter.SelectCommand = command;
+                    dataAdapter = new SqlDataAdapter();
+                    dataAdapter.SelectCommand = command;
 
                     // Создать CommandBuilder.
-                    SqlCommandBuilder builder = new SqlCommandBuilder();
-                    builder.DataAdapter = adapter;
+                    builder = new SqlCommandBuilder();
+                    builder.DataAdapter = dataAdapter;
                     // Получить команду на вставку
-                    adapter.InsertCommand = builder.GetInsertCommand();
+                    dataAdapter.InsertCommand = builder.GetInsertCommand();
 
                     DataTable table = ds.Tables["Fuels"];
 
-                    adapter.Update(table);
+                    dataAdapter.Update(table);
                     table.AcceptChanges();
 
                 }
 
                 labelInfo.Text = labelInfo.Text + "Добавлено!!!\r\n";
                 labelInfo.Refresh();
+                DisplayFuels(textBoxFindFuels.Text);
+
 
             }
             catch (Exception exeption)
